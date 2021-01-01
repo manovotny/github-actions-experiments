e24333dbe54fcdeaec96ed621c144b88c987e45c
Michael Novotny <manovotny@gmail.com>
2021-01-01 13:31:21 -0600

    Adds console logs.

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -1,9 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const dotProp = require('dot-prop');
+const fs = require('fs-extra');
 
-try {
-    const stringified = JSON.stringify(github, undefined, 2);
-    console.log('GITHUB', stringified);
-} catch (error) {
-    core.setFailed(error.message);
-}
+(async () => {
+    try {
+        const stringified = JSON.stringify(github, undefined, 2);
+        console.log('GITHUB', stringified);
+
+        const {draft, prerelease, tag_name} = github.context.payload.release;
+        console.log('DRAFT', draft);
+        console.log('PRERELEASE', prerelease);
+        console.log('TAG', tag_name);
+
+        const packageJson = await fs.readJson('./package.json');
+        const version = dotProp.get(packageJson, 'version', undefined);
+        const build = dotProp.get(packageJson, 'scripts.build', undefined);
+        console.log('VERSION', version);
+        console.log('BUILD', build);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
