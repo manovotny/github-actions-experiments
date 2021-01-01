c84c2ef1fc9cdedd76678c79e047f0719fd2d830
Michael Novotny <manovotny@gmail.com>
2021-01-01 17:01:36 -0600

    Adds output from release step.

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -2,22 +2,42 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const dotProp = require('dot-prop');
 const fs = require('fs-extra');
+const semver = require('semver');
 
 (async () => {
     try {
-        const stringified = JSON.stringify(github, undefined, 2);
-        console.log('GITHUB', stringified);
-
-        const {draft, prerelease, tag_name} = github.context.payload.release;
-        console.log('DRAFT', draft);
-        console.log('PRERELEASE', prerelease);
-        console.log('TAG', tag_name);
-
+        const {draft, prerelease, tag_name: releaseVersion} = github.context.payload.release;
+        const releaseVersionWithoutV = releaseVersion.substring(1);
         const packageJson = await fs.readJson('./package.json');
-        const version = dotProp.get(packageJson, 'version', undefined);
-        const build = dotProp.get(packageJson, 'scripts.build', undefined);
-        console.log('VERSION', version);
-        console.log('BUILD', build);
+        const packageJsonVersion = dotProp.get(packageJson, 'version', undefined);
+        const packageJsonBuildScript = dotProp.get(packageJson, 'scripts.build', undefined);
+
+        if (!releaseVersion.startsWith('v')) {
+            core.setFailed('Release tag does not start with `v`, ie. `v1.2.3`.');
+        }
+
+        if (releaseVersionWithoutV !== packageJsonVersion) {
+            core.setFailed(`
+                Release version does not match package.json version.
+                Release version: ${releaseVersionWithoutV}
+                Package.json version: ${packageJsonVersion}
+            `);
+        }
+
+        if (!semver.valid(releaseVersionWithoutV)) {
+            core.setFailed(`Release / Package.json version is not valid semver.`);
+        }
+
+        const prereleaseTag = semver.prerelease(releaseVersionWithoutV);
+        let tag = '';
+
+        if (prereleaseTag !== null) {
+            tag = ` --tag ${prereleaseTag[0]}`;
+        }
+
+        const command = `yarn publish --new-version ${releaseVersionWithoutV}${tag}`;
+        console.log('COMMAND', command);
+        core.setOutput('command', command);
     } catch (error) {
         core.setFailed(error.message);
     }
